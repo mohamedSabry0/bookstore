265eac2108564b583faaa266c6d683538613329b
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-03-06 13:27:03 +0200

    init redux

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = { value: [] }
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    removeBook(state, action) {
+      state.value.splice(action.payload)
+    },
+    AddBook(state, action) {
+      state.value.push(action.payload)
+    },
+  },
+})
+
+export default booksSlice.reducer;
