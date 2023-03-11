c78feaa8b5bb5fe91fd1b926c69f01fdfdba88c7
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-03-11 20:45:09 +0200

    add category to the book card

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,14 +2,17 @@ import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/booksSlice';
 
-function Book({ id, title, author }) {
+function Book({
+  id, title, author, category,
+}) {
   const dispatch = useDispatch();
   const removeHandler = () => {
     dispatch(removeBook(id));
   };
   return (
-    <div id={id} className="book">
+    <div className="book">
       <div>
+        <span className="book-category">{category}</span>
         <span className="book-title">{title}</span>
         <span className="book-author">{author}</span>
 
@@ -34,6 +37,7 @@ function Book({ id, title, author }) {
 
 Book.propTypes = {
   id: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
 };
